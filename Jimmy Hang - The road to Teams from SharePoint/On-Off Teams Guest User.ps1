a9492d590b7cfc9f76b4fac83118952ed9bbbcf2
JimmyHang <jimmy.hang@rewired.no>
2019-02-13 00:39:37 +0100

    NICCONF presentation and demo scripts

---
+++ Jimmy Hang - The road to Teams from SharePoint/On-Off Teams Guest User.ps1
@@ -0,0 +1,47 @@
+﻿#By default if enabled Guest access will be ON for all Teams
+#Follow the steps below to turn Guest Access ON/OFF for selected teams
+#Tenant and Group details
+
+$tenantId = "TenantID"
+$groupName = "Contract Management"
+
+
+#Connect to Exchange and Azure AD
+$cred = Get-Credential
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $cred -Authentication Basic -AllowRedirection
+Import-PSSession $session
+Connect-AzureAD -TenantId $tenantId -Credential $cred
+
+#Turn OFF guest access
+$template = Get-AzureADDirectorySettingTemplate | ? {$_.displayname -eq "group.unified.guest"}
+$settingsCopy = $template.CreateDirectorySetting()
+$settingsCopy["AllowToAddGuests"]=$False
+
+$groupID= (Get-AzureADGroup -SearchString $groupName).ObjectId
+New-AzureADObjectSetting -TargetType Groups -TargetObjectId $groupID -DirectorySetting $settingsCopy
+
+#Verify settings
+Get-AzureADObjectSetting -TargetObjectId $groupID -TargetType Groups | Format-List Values
+
+
+######
+#Turn ON guest access
+#Remove current setting
+$groupID = (Get-AzureADGroup -SearchString $groupName).ObjectId
+$settingId = (Get-AzureADObjectSetting -TargetObjectId $groupID -TargetType Groups).Id
+Remove-AzureADObjectSetting -TargetType Groups -TargetObjectId $groupID -Id $settingId
+
+#Verify settings | it should be empty
+Get-AzureADObjectSetting -TargetObjectId $groupID -TargetType Groups | fl Values
+
+#Alternatively you can add a new template with AllowToAddGuest = true
+$template = Get-AzureADDirectorySettingTemplate | ? {$_.displayname -eq "group.unified.guest"}
+$settingsCopy = $template.CreateDirectorySetting()
+$settingsCopy["AllowToAddGuests"]=$True
+
+$groupID= (Get-AzureADGroup -SearchString $groupName).ObjectId
+New-AzureADObjectSetting -TargetType Groups -TargetObjectId $groupID -DirectorySetting $settingsCopy
+
+#Verify settings
+Get-AzureADObjectSetting -TargetObjectId $groupID -TargetType Groups | Format-List Values
+
---
+++ Jimmy Hang - The road to Teams from SharePoint/SetAllowGroupCreation.ps1
@@ -0,0 +1,37 @@
+﻿#Group creation policy
+#Run the script below to make sure only approved users may create new groups 
+#https://docs.microsoft.com/en-us/office365/admin/create-groups/manage-creation-of-groups?view=o365-worldwide
+
+$GroupName = "AllowGroupCreation"
+$AllowGroupCreation = "True"
+
+#Connect-AzureAD
+
+try
+    {
+        $template = Get-AzureADDirectorySettingTemplate | ? {$_.displayname -eq "group.unified"}
+        $settingsCopy = $template.CreateDirectorySetting()
+        New-AzureADDirectorySetting -DirectorySetting $settingsCopy
+        $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+    }
+catch
+    {
+        $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id       
+    }
+
+$settingsCopy = Get-AzureADDirectorySetting -Id $settingsObjectID
+
+$settingsCopy["EnableGroupCreation"] = $AllowGroupCreation
+
+If ($GroupName -eq "")
+    {
+        $SettingsCopy["GroupCreationAllowedGroupId"] = ""
+    }
+Else
+    {
+        $SettingsCopy["GroupCreationAllowedGroupId"] = (Get-AzureADGroup -SearchString $GroupName).objectid
+    }
+
+Set-AzureADDirectorySetting -Id $settingsObjectID -DirectorySetting $settingsCopy
+
+(Get-AzureADDirectorySetting -Id $settingsObjectID).Values
